5022babf2b1388e3e7c0ed3e0f3085ca42f3f3aa
junyaokh822 <junyaokh822@gmail.com>
2023-06-26 14:25:28 -0400

    update

--- day10-react-state-and-events/job-app-tracker-starter/.eslintrc.cjs
+++ day10-react-state-and-events/job-app-tracker-starter/.eslintrc.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   env: { browser: true, es2020: true },
+  plugins: ['react-refresh','testing-library', 'jest-dom', 'vitest'],
   extends: [
+    'react-app',
+    'react-app/jest',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+    'plugin:vitest/recommended',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
@@ -8,7 +14,6 @@ module.exports = {
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
