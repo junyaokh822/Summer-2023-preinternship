4ac19b95fdda55fadea3ccb9bfd26ab898851d58
junyaokh822 <junyaokh822@gmail.com>
2023-07-06 12:16:32 -0400

    wrap up today lesson

--- day17-intro-to-node-and-express-requests-and-routing/job-application-tracker-api/server.js
+++ day17-intro-to-node-and-express-requests-and-routing/job-application-tracker-api/server.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const app = express();
 const port = 4000;
-const jobs=require("./jobs");
+const axios = require('axios');
+require('dotenv').config();
+
+app.get('/recipes', async (req, res) => {
+  try {
+    const apiKey = process.env.SPOONACULAR_API_KEY;
+    const url = `https://api.spoonacular.com/recipes/complexSearch`;
+
+    // Pass parameters from the Express request to the Spoonacular API
+    const { data } = await axios.get(url, {
+      params: {
+        ...req.query,
+        apiKey: apiKey
+      }
+    });
+
+    // Send the response from the Spoonacular API back to the client
+    res.json(data);
+  } catch (error) {
+    res.status(500).send('An error occurred');
+  }
+});
 
 app.use((req, res, next) => {
-  console.log(`Request: ${req.method} ${req.originalUrl}`);
+  console.log(`Request: ${req.method} ${req.originalUrl} ${res.statusCode}`);
   next();
 });
 app.use(express.json());
@@ -26,40 +47,46 @@ app.get("/jobs", (req, res) => {
 
 // Get a specific job
 app.get("/jobs/:id", (req, res) => {
-  const jobId = parseInt(req.params.id, 10);
+  const jobId = parseInt(req.params.id);
   const job = jobs.find((job) => job.id === jobId);
-  res.send(job);
+  if (job) {
+    res.send(job);
+  } else {
+    res.status(404).send({ message: "Job not found" });
+  }
 });
 
 // Create a new job
 app.post("/jobs", (req, res) => {
-  // This will eventually create a new job
-  const newJob = {
-    ...req.body,
-    id: getNextIdFromCollection(jobs)
-  };
-  console.log("newJob", newJob);
+  const newJob = req.body;
   jobs.push(newJob);
-  res.send(newJob);
+  res.status(201).send(newJob);
 });
 
 // Update a specific job
 app.patch("/jobs/:id", (req, res) => {
   const jobId = parseInt(req.params.id, 10);
   const jobUpdates = req.body;
-  const jobIndex = jobs.findIndex(job => job.id === jobId);
+  const jobIndex = jobs.findIndex((job) => job.id === jobId);
   const updatedJob = { ...jobs[jobIndex], ...jobUpdates };
-  jobs[jobIndex] = updatedJob;
-  // console.log("updatedJob", updatedJob);
-  res.send(updatedJob);
+  if (jobIndex !== -1) {
+    jobs[jobIndex] = updatedJob;
+    res.send(updatedJob);
+  } else {
+    res.status(404).send({ message: "Job not found" });
+  }
 });
 
 // Delete a specific job
 app.delete("/jobs/:id", (req, res) => {
   const jobId = parseInt(req.params.id, 10);
-  const jobIndex = jobs.findIndex(job => job.id === jobId);
-  jobs.splice(jobIndex, 1);
-  res.send({ message: "Job deleted successfully" });
+  const jobIndex = jobs.findIndex((job) => job.id === jobId);
+  if (jobIndex !== -1) {
+    jobs.splice(jobIndex, 1);
+    res.send({ message: "Job deleted successfully" });
+  } else {
+    res.status(404).send({ message: "Job not found" });
+  }
 });
 
 app.listen(port, () => {
