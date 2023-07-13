ec8152e90ca950b3e6c3abbd5e5f1ea443910cce
Junyao <junyaokh822@gmail.com>
2023-07-13 11:51:16 -0400

    updates

--- day22-node-and-express-pt-5-authentication-and-access-control/job-application-tracker-api/server.js
+++ day22-node-and-express-pt-5-authentication-and-access-control/job-application-tracker-api/server.js
@@ -64,7 +64,41 @@ app.post("/signup", async (req, res) => {
 });
 
 
+//login
+app.post('/login', async (req, res) => {
+  try {
+    // First, find the user by their email address
+    const user = await User.findOne({ where: { email: req.body.email } });
 
+    if (user === null) {
+      // If the user isn't found in the database, return an 'incorrect credentials' message
+      return res.status(401).json({
+        message: 'Incorrect credentials',
+      });
+    }
+
+    // If the user is found, we then use bcrypt to check if the password in the request matches the hashed password in the database
+    bcrypt.compare(req.body.password, user.password, (error, result) => {
+      if (result) {
+        // Passwords match
+        // TODO: Create a session for this user
+
+        res.status(200).json({
+          message: 'Logged in successfully',
+          user: {
+            name: user.name,
+            email: user.email,
+          },
+        });
+      } else {
+        // Passwords don't match
+        res.status(401).json({ message: 'Incorrect credentials' });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred during the login process' });
+  }
+});
 
 
 
