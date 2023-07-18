676e8b74a52aee612b7cd17dcd0eeb8a8c5eb6a8
Junyao <junyaokh822@gmail.com>
2023-07-18 10:42:24 -0400

    updates

--- day24-node-express-and-react-part2/job-app-tracker-client/src/routes/root.jsx
+++ day24-node-express-and-react-part2/job-app-tracker-client/src/routes/root.jsx
@@ -1,33 +1,13 @@
-import { useEffect, useContext } from "react";
-import { Link, Outlet, useNavigation, useLoaderData, Form } from "react-router-dom";
+// src/routes/root.jsx
+import { useContext } from "react";
+import { Link, Outlet, useNavigation, Form, redirect } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import classNames from "classnames";
 import { AuthContext } from "../contexts/AuthContext";
 
-export async function loader({ request }) {
-  const response = await fetch("/api/auth/current_user");
-  if (response.ok) {
-    const { user } = await response.json();
-    return { currentUser: user };
-  }
-  return { currentUser: null };
-}
-
-export async function action({ request }) {
-  const response = await fetch("/api/auth/logout", {
-    method: "DELETE"
-  });
-  return null;
-}
-
 function Root() {
-  const { currentUser } = useLoaderData();
-  const { setCurrentUser } = useContext(AuthContext);
+  const { currentUser, logout } = useContext(AuthContext);
   const navigation = useNavigation();
-
-  useEffect(() => {
-    setCurrentUser(currentUser);
-  }, [currentUser]);
 
   const outletClasses = classNames(
     "mx-auto max-w-4xl sm:px-12 px-4 transition-opacity",
@@ -36,6 +16,13 @@ function Root() {
       "opacity-50": navigation.state === "loading",
     }
   );
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    logout();
+    redirect("/login");
+  };
+
   return (
     <div>
       <nav className="bg-blue-900 h-14 flex justify-items-center justify-between">
@@ -46,11 +33,13 @@ function Root() {
           </Link>
         </h2>
         <div className="flex items-center pr-2">
-          <Form method="post">
-            <button type="submit" className="">
-              Logout
-            </button>
-          </Form>
+          {currentUser && (
+            <Form method="post" onSubmit={handleLogout}>
+              <button type="submit" className="">
+                Logout
+              </button>
+            </Form>
+          )}
         </div>
       </nav>
 
