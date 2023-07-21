1c81032a65b23f1ca11cf222f5e403d9d1427c7a
Junyao <junyaokh822@gmail.com>
2023-07-21 12:57:52 -0400

    updates

--- day27-breadth-first-search-and-two-pointers/bfs.js
+++ day27-breadth-first-search-and-two-pointers/bfs.js
@@ -2,6 +2,7 @@ class Node {
   constructor(value, children=[]) {
     this.value = value;
     this.children = children;
+    this.next=null;
   }
 }
 
@@ -15,8 +16,8 @@ class Queue {
     return !this.front;
   }
 
-  add(value) {
-    const newNode = new Node(value);
+  add(node) {
+    const newNode = node;
     if (this.isEmpty()) {
       this.front = this.back = newNode;
     } else {
@@ -29,14 +30,33 @@ class Queue {
     if (this.isEmpty()) {
       return null;
     }
-    const value = this.front.value;
+    const front = this.front;
     this.front = this.front.next;
-    return value;
+    return front;
   }
 }
 
 function bfsTree(root, searchValue) {
-  
+  let queue = new Queue();
+  queue.add(root);
+
+  while (!queue.isEmpty()) {
+    let currentNode = queue.remove();
+
+    console.log("Visiting node: ", currentNode.value);
+
+    if (currentNode.value === searchValue) {
+      console.log("Found the node!");
+      return currentNode;
+    }
+
+    for (let child of currentNode.children) {
+      queue.add(child);
+    }
+  }
+
+  console.log("Node not found!");
+  return null;
 }
 
 
@@ -69,7 +89,32 @@ class Graph {
 }
 
 function bfsGraph(graph, startValue, searchValue) {
+  let startVertex = graph.getVertex(startValue);
+  let queue = new Queue();
+  queue.add(startVertex);
   
+  let visited = new Set();
+  visited.add(startVertex);
+
+  while (!queue.isEmpty()) {
+    let currentVertex = queue.remove();
+
+    if (currentVertex.value === searchValue) {
+      console.log("Found the vertex!");
+      return currentVertex;
+    }
+
+    for (let neighbor of currentVertex.neighbors) {
+      //if we have not visited a neighbor then queue it up and mark it as visited
+      if (!visited.has(neighbor)) {
+        queue.add(neighbor);
+        visited.add(neighbor);
+      }
+    }
+  }
+
+  console.log("Vertex not found!");
+  return null;
 }
 
 module.exports = {
