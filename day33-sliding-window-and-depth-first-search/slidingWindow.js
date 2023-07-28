445a46b953290b64a9556cbcff7106c169bfb663
Junyao <junyaokh822@gmail.com>
2023-07-28 11:10:44 -0400

    updates for sliding window

--- day33-sliding-window-and-depth-first-search/slidingWindow.js
+++ day33-sliding-window-and-depth-first-search/slidingWindow.js
@@ -1,5 +1,21 @@
 function maxSubArrayOfSizeK(arr, k) {
-  
+    let maxSum = 0;
+    let windowSum = 0;
+    let windowStart = 0;
+    for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+        const windowWidth= windowEnd - windowStart +1;
+        //we add the windowSum until we hit the window size
+        windowSum += arr[windowEnd];
+        //after we exceed the window size, we also need to remove the 
+        //element that is no longer in the window
+        if (windowWidth > k  ) {
+            windowSum -= arr[windowStart]; 
+            windowStart++;
+            maxSum = Math.max(maxSum, windowSum);
+        }else if (windowWidth === k){
+            maxSum = windowSum;
+        }
+    }
+    return maxSum;
 }
-
 console.log(maxSubArrayOfSizeK([2, 1, 5, 1, 3, 2], 3));
